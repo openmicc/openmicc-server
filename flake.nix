5b1fffdfd78402d843fdd60898c061776d4d3dd8
Oliver Evans <oliverevans96@gmail.com>
2022-11-06 00:07:39 -0700

    Update flake to work (or at least almsot) with buildRustPackage

--- flake.nix
+++ flake.nix
@@ -2,9 +2,26 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nocargo = {
+      url = "github:oxalica/nocargo";
+      inputs.nixpkgs.follows = "nixpkgs";
+
+      # See below.
+      inputs.registry-crates-io.follows = "registry-crates-io";
+    };
+
+    # Optionally, you can explicitly import crates.io-index here.
+    # So you can `nix flake update` at any time to get cutting edge version of crates,
+    # instead of waiting `nocargo` to dump its dependency.
+    # Otherwise, you can simply omit this to use the locked registry from `nocargo`,
+    # which is updated periodically.
+    registry-crates-io = {
+      url = "github:rust-lang/crates.io-index";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nocargo, registry-crates-io }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -78,6 +95,46 @@
           '';
           installPhase = "cp -r . $out";
         };
+        nocargo-pkg = nocargo.lib.${system}.mkRustPackageOrWorkspace {
+          # The root directory, which contains `Cargo.lock` and top-level `Cargo.toml`
+          # (the one containing `[workspace]` for workspace).
+          src = ./.;
+
+          # If you use registries other than crates.io, they should be imported in flake inputs,
+          # and specified here. Note that registry should be initialized via `mkIndex`,
+          # with an optional override.
+          extraRegistries = {
+            # "https://example-registry.org" = nocargo.lib.${system}.mkIndex inputs.example-registry {};
+          };
+
+          # If you use crates from git URLs, they should be imported in flake inputs,
+          # and specified here.
+          gitSrcs = {
+            # "https://github.com/some/repo" = inputs.example-git-source;
+          };
+
+          # If some crates in your dependency closure require packages from nixpkgs.
+          # You can override the argument for `stdenv.mkDerivation` to add them.
+          #
+          # Popular `-sys` crates overrides are maintained in `./crates-io-override/default.nix`
+          # to make them work out-of-box. PRs are welcome.
+          buildCrateOverrides = with pkgs;
+            {
+              # Use package id format `pkgname version (registry)` to reference a direct or transitive dependency.
+              # "zstd-sys 2.0.1+zstd.1.5.2 (registry+https://github.com/rust-lang/crates.io-index)" =
+              #   old: {
+              #     nativeBuildInputs = [ pkg-config ];
+              #     propagatedBuildInputs = [ zstd ];
+              #   };
+
+              # Use package name to reference local crates.
+              # "mypkg1" = old: { nativeBuildInputs = [ git ]; };
+            };
+
+          # We use the rustc from nixpkgs by default.
+          # But you can override it, for example, with a nightly version from https://github.com/oxalica/rust-overlay
+          # rustc = rust-overlay.packages.${system}.rust-nightly_2022-07-01;
+        };
 
       in rec {
         packages.mediasoup = patched-mediasoup;
@@ -101,11 +158,16 @@
           ];
         };
 
+        packages.nocargo = nocargo-pkg.release.openmicc-server;
+
         defaultPackage = rustPlatform.buildRustPackage {
           pname = "openmicc-server";
           version = "0.1.0";
 
           nativeBuildInputs = with pkgs; [ lld pkgconfig udev meson ninja ];
+
+          buildInputs = with pkgs; [ openssl zstd ];
+
           dontUseNinjaBuild = true;
           dontUseNinjaCheck = true;
           dontUseNinjaInstall = true;
@@ -117,9 +179,13 @@
             cp ${patched-cargo-lock} Cargo.lock
           '';
 
+          buildType = "debug";
+          doCheck = false;
+
           preBuild = ''
             cp -r ${mediasoup-wrap-patched} ${patched-mediasoup-rel-path}
             chmod u+w -R ${patched-mediasoup-rel-path}
+            # chmod u+w -R ${patched-src}
           '';
 
           # # patches = [ cargo-toml-patch ];
@@ -132,7 +198,7 @@
           src = ./.;
 
           # build-time deps
-          nativeBuildInputs = (with pkgs; [
+          nativeBuildInputs = with pkgs; [
             python
             pythonPackages.pip
             rustc
@@ -142,7 +208,9 @@
             udev
             meson
             ninja
-          ]);
+          ];
+
+          buildInputs = with pkgs; [ openssl zstd ];
         };
       });
 }
